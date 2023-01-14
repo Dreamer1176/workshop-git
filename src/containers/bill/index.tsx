96880988c067fbb07db0f6c9e1dbb8e8de98beea
Dreamer1176 <98012273+Dreamer1176@users.noreply.github.com>
2023-01-14 16:18:58 +0700

    show bill grand total price

--- src/containers/bill/index.tsx
+++ src/containers/bill/index.tsx
@@ -63,9 +63,26 @@ const BillContainer = (): ReactElement => {
           </Row>
         </StyledSubHeaderDetail>
         {/* Content */}
-        <Table columns={columns} dataSource={dataProducts} pagination={false} />
+        <Table 
+        columns={columns} 
+        dataSource={dataProducts} 
+        pagination={false} 
+        summary={(data) => {
+          const total = data.reduce((prev, curr) => prev + curr.price,0)
+          return(
+            <Table.Summary>
+              <Table.Summary.Row>
+                <Table.Summary.Cell index = {0} colSpan={3} align={'right'}>
+                   จำนวนเงิน
+                </Table.Summary.Cell>
+              </Table.Summary.Row>
+            </Table.Summary>
+          )
+        }}
+        />
       </StyledWrapper>
     </StyledLayout>
   )
 }
 export default BillContainer
+
